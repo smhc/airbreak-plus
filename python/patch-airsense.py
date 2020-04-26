d4a123985bee929267c1d2cac238189d52ecbaee
Colin O'Flynn <coflynn@newae.com>
2020-04-26 13:28:53 -0300

    Fixing Python patch utility, remove image stuff for now

--- python/patch-airsense.py
+++ python/patch-airsense.py
@@ -9,7 +9,10 @@
 # warranties of merchantability, non-infringement of third party rights, or fitness for a
 # particular purpose.
 #
+# See LICENSE in main repository for distribution license and additional restrictions.
 
+import argparse
+import hashlib
 import crcmod
 import crcmod.predefined
 import os.path
@@ -29,8 +32,10 @@ class ASFirmware(object):
         self.validate()
         
     def validate(self):
-        """Validate the input file looks OK"""
-            
+        """Validate the input file looks OK and populate information"""
+        
+        self.hash = hashlib.sha256(bytes(self.fw)).hexdigest()
+        
         #Step 1: Check CRCs seem OK
         crc1 = self.crcfunc(bytes(self.fw[0x0:0x4000]))
         crc2 = self.crcfunc(bytes(self.fw[0x4000:0x40000]))
@@ -82,7 +87,7 @@ class ASFirmware(object):
 
         return i1
 
-    def patch(self, patchdata, addr=None, dataseq=None, hash=None, verbose=True, checkreserved=True, checkempty=False, clobber=True):
+    def patch(self, patchdata, addr=None, dataseq=None, hash=None, verbose=True, checkreserved=True, checkempty=False, clobber=False):
         """Updates firmware data with patchdata, based on address, sequence, or hash of sequence"""
         
         #I love Python3(TM)
@@ -226,137 +231,223 @@ class ASFirmware(object):
         addr_offset = int(addr_offset, 16)
         
         return addr_offset, bin
+
+class ASUnits(object):
+    def __init__(self, name, catalog_no, sw_ver, hash):
+        self.name = name
+        self.catalog_no = catalog_no
+        self.sw_ver = sw_ver
+        self.hash = hash
+
+class ASFirmwarePatches(object):
+    """This class contains the actual patching scripts for specific items"""
+
+    known_units = [
+        ASUnits("AirSense 10 Autoset", "37028", "SX567-0401", "533b91127aa22e05b933db203ad56c449dc12a8c3fd62f57bd88c472a8061775"),
+    ]
         
+    def __init__(self, asf):
+        self.asf = asf
+
+    def bypass_startcheck(self):
+        #Start-up check for CRC etc, bypass it to avoid (might not be needed)
+        if self.asf.hash == self.known_units[0].hash:
+            asf.patch(b'\xc0\x46', 0xF0, clobber=True)
+        else:
+            raise IOError("Unknown hash: %s"%self.asf.hash)
+            
+    def change_text(self):
+        if self.asf.hash == self.known_units[0].hash:
+            asf.patch(b'HACKED!', 0x17500, clobber=True)
+            asf.patch(b'NOT FOR USE\x00', 0x1A540, clobber=True)
+            asf.patch(b'WARNING! WARNING! Ventilator test firmware: Not for humans!\x00', 0x1B860, clobber=True)
+        else:
+            raise IOError("Unknown hash: %s"%self.asf.hash)
+
+    def unlock_ui_limits(self):
         
+        if self.asf.hash == self.known_units[0].hash:
+            self.asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fa8, clobber=True)
+            self.asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fc4, clobber=True)
+            self.asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7eb0, clobber=True)
+            self.asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ee8, clobber=True)
+            self.asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ecc, clobber=True)
+        else:
+            raise IOError("Unknown hash: %s"%self.asf.hash)
 
-def unlock_ui_limits():
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fa8, clobber=True)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x4fc4, clobber=True)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7eb0, clobber=True)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ee8, clobber=True)
-    asf.patch(b'\xdc\x05\x00\x00\x32\x00', 0x7ecc, clobber=True)
+    def extra_debug(self):
+        # set config variable 0xc value to 4 == enable more debugging data on display
+        # if you set it to \x0f it will enable four separate display pages of info in sleep report mode
+        if self.asf.hash == self.known_units[0].hash:
+            self.asf.patch(b'\x04', 0x84a8, clobber=True)
+        else:
+            raise IOError("Unknown hash: %s"%self.asf.hash)
 
+    def extra_modes(self):
+        # add more mode entries, set config 0x0 mask to all bits high
+        # default is 0x3, which only enables mode 1 (CPAP) and 2 (AutoSet)
+        # ---> This is the real magic <---
+        if self.asf.hash == self.known_units[0].hash:
+            self.asf.patch(b'\xff\xff', 0x8590, clobber=True)
+        else:
+            raise IOError("Unknown hash: %s"%self.asf.hash)
 
-def extra_debug():
-    # set config variable 0xc value to 4 == enable more debugging data on display
-    # if you set it to \x0f it will enable four separate display pages of info in sleep report mode
-    asf.patch(b'\x04', 0x84a8, clobber=True)
+    def extra_menu(self):
+        #try enabling extra menu items
+        if self.asf.hash == self.known_units[0].hash:
+            self.asf.patch(b'\x01\x20', 0x66470, clobber=True)
+        else:
+           raise IOError("Unknown hash: %s"%self.asf.hash)
 
-def extra_modes():
-    # add more mode entries, set config 0x0 mask to all bits high
-    # default is 0x3, which only enables mode 1 (CPAP) and 2 (AutoSet)
-    # ---> This is the real magic <---
-    asf.patch(b'\xff\xff', 0x8590, clobber=True)
-
-def extra_menu():
-    #try enabling extra menu items
-    asf.patch(b'\x01\x20', 0x66470, clobber=True)
-
-# If you want all menu items to always be visible, let this section run
-def all_menu():
-    # force status bit 5 always on -- always editable
-    asf.patch(b'\x01\x20', 0x6e502, clobber=True)
-    # force status bit 4 always on -- this makes all the inputs show up, regardless of mode
-    asf.patch(b'\x01\x20', 0x6e4c4, clobber=True)
-
-
-def gui_config ():
-    # enable all of the editable options in the settings menu
-    # by turning on bit 1 of the config entries.  All of these variables
-    # are listed in the gui_create_menus function
-    GUI_CONFIG=0x4ef4
-    GUI_CONFIG_SIZE=0x1c
-    GUI_CONFIG_OFFSET=30
     
-    for var in [0x2f, 0x1ec, 0x1ed, 0x24, 0x25, 0x1d3,
-                0x1d6, 0x1d5, 0x1d7, 0x26, 0x1d9, 0x1e0,
-                0x1e1, 0x1e2, 0x1e5, 0x1e4, 0x1e6, 0x1e7,
-                0x1e9, 0x1ea, 0x1eb]:
-        addr = GUI_CONFIG + (var - GUI_CONFIG_OFFSET) * GUI_CONFIG_SIZE
-        asf.patch(b'\x07\x00', addr)
+    def all_menu(self):
+        if self.asf.hash == self.known_units[0].hash:
+            # If you want all menu items to always be visible, let this section run
+            # force status bit 5 always on -- always editable
+            self.asf.patch(b'\x01\x20', 0x6e502, clobber=True)
+            # force status bit 4 always on -- this makes all the inputs show up, regardless of mode
+            self.asf.patch(b'\x01\x20', 0x6e4c4, clobber=True)
+        else:
+           raise IOError("Unknown hash: %s"%self.asf.hash)
 
-def patch_logos():
+    def gui_config (self):
+        # enable all of the editable options in the settings menu
+        # by turning on bit 1 of the config entries.  All of these variables
+        # are listed in the gui_create_menus function
+        
+        if self.asf.hash == self.known_units[0].hash:
+            GUI_CONFIG=0x4ef4
+            GUI_CONFIG_SIZE=0x1c
+            GUI_CONFIG_OFFSET=30
+            
+            for var in [0x2f, 0x1ec, 0x1ed, 0x24, 0x25, 0x1d3,
+                        0x1d6, 0x1d5, 0x1d7, 0x26, 0x1d9, 0x1e0,
+                        0x1e1, 0x1e2, 0x1e5, 0x1e4, 0x1e6, 0x1e7,
+                        0x1e9, 0x1ea, 0x1eb]:
+                addr = GUI_CONFIG + (var - GUI_CONFIG_OFFSET) * GUI_CONFIG_SIZE
+                self.asf.patch(b'\x07\x00', addr, clobber=True)
+        else:
+           raise IOError("Unknown hash: %s"%self.asf.hash)
 
-    #Change these to adjust logos, rest should work automatically.
-    #NB - be sure of settings when saving file:
-    #     'text' was exported with `Compressed, RLE4`
-    #     'logo' was exported with `Compressed, RLE8`
-    import image_conversion_example.example1_umbrella_logo_c as logo
-    import image_conversion_example.example1_umbrella_text_c as text
-    
-    # Find somewhere to stash our stuff in the flash memory
-    # NOTE: Pallet is in 32-bit, and need room for struct stuff around pallete
-    pallete_addr = asf.find_flash_room(len(logo.pallete*4)+32, reserve=True) 
-    pixels_addr = asf.find_flash_room(len(logo.pixels), reserve=True)
-    
-    # Find the location of the original wave
-    setting_loc = asf.find_bytes([0xb8, 0x00, 0x54, 0x00, 0xb8, 0x00, 0x00, 0x00])
-    
-    asf.patch_image(setting_loc, pallete_addr, pixels_addr, logo)    
-    
-    # Find somewhere to stash our stuff in the flash memory
-    # NOTE: Pallet is in 32-bit, and need room for struct stuff around pallete
-    pallete_addr = asf.find_flash_room(len(text.pallete*4)+32, reserve=True) 
-    pixels_addr = asf.find_flash_room(len(text.pixels), reserve=True)
-    
-    # Find the location of the original text
-    setting_loc = asf.find_bytes([0xB8, 0x00, 0x32, 0x00, 0x5c, 0x00, 0x00])
-    
-    asf.patch_image(setting_loc, pallete_addr, pixels_addr, text)
+    def patch_logos(self):
 
-def patch_uart3_monitor():
+        #Change these to adjust logos, rest should work automatically.
+        #NB - be sure of settings when saving file:
+        #     'text' was exported with `Compressed, RLE4`
+        #     'logo' was exported with `Compressed, RLE8`
+        import image_conversion_example.example1_umbrella_logo_c as logo
+        import image_conversion_example.example1_umbrella_text_c as text
+        
+        # Find somewhere to stash our stuff in the flash memory
+        # NOTE: Pallet is in 32-bit, and need room for struct stuff around pallete
+        pallete_addr = self.asf.find_flash_room(len(logo.pallete*4)+32, reserve=True) 
+        pixels_addr = self.asf.find_flash_room(len(logo.pixels), reserve=True)
+        
+        # Find the location of the original wave
+        setting_loc = self.asf.find_bytes([0xb8, 0x00, 0x54, 0x00, 0xb8, 0x00, 0x00, 0x00])
+        
+        asf.patch_image(setting_loc, pallete_addr, pixels_addr, logo)    
+        
+        # Find somewhere to stash our stuff in the flash memory
+        # NOTE: Pallet is in 32-bit, and need room for struct stuff around pallete
+        pallete_addr = self.asf.find_flash_room(len(text.pallete*4)+32, reserve=True) 
+        pixels_addr = self.asf.find_flash_room(len(text.pixels), reserve=True)
+        
+        # Find the location of the original text
+        setting_loc = self.asf.find_bytes([0xB8, 0x00, 0x32, 0x00, 0x5c, 0x00, 0x00])
+        
+        self.asf.patch_image(setting_loc, pallete_addr, pixels_addr, text)
 
+    def patch_uart3_monitor(self):
 
-    irq_offset, irq_bin = asf.prepare_bin("../serial_monitor/monitor_irq")
-    
-    #Need to rebuild if location changes - for now just fix it
-    #irq_location = asf.find_flash_room(len(data)*2)
-    #print("Suggest to place at %x"%irq_location)
-    irq_location = 0xC600
-    asf.patch(irq_bin, irq_location, checkempty=True)
-    
-    init_offset, init_bin = asf.prepare_bin("../serial_monitor/monitor_init")
-    if init_offset != 0:
-        raise ValueError("Nonsense - no other function!?")
-    
-    #Init location
-    init_location = asf.find_bytes([0x70, 0xb5, 0x84, 0xb0, 0x04, 0x46, 00, 0xf0])
-    if (init_location != 0xC339A):
-        raise ValueError("oops.... should rebuild this")
-    asf.patch(init_bin, init_location, clobber=True)
-    
-    #Entry is not at start of file sometimes in this file?
-    print("IRQ has offset of 0x%x (dealt with)"%irq_offset)
-    irq_location += irq_offset
-    
-    # IRQ vector - at fixed location 0x080402DC so don't need to worry about
-    # this moving. Address needs to be +1 for normal code jump location.
-    irq_location_packed = struct.pack("<I", 0x08000000 + irq_location + 1)
-    asf.patch(irq_location_packed, 0x402dc, clobber=True)
+        irq_offset, irq_bin = self.asf.prepare_bin("../serial_monitor/monitor_irq")
+        
+        # Need to rebuild if location changes - for now just fix it, check we've got room
+        # before doing it.
+        #
+        # If following fails, these two lines will figure out where there is room again
+        # irq_location = asf.find_flash_room(len(data)*2)
+        # print("Suggest to place at %x"%irq_location)
+        irq_location = 0xC600
+        asf.patch(irq_bin, irq_location, checkempty=True)
+        
+        init_offset, init_bin = self.asf.prepare_bin("../serial_monitor/monitor_init")
+        if init_offset != 0:
+            raise ValueError("Nonsense - no other function!?")
+        
+        #Init location
+        init_location = self.asf.find_bytes([0x70, 0xb5, 0x84, 0xb0, 0x04, 0x46, 00, 0xf0])
+        if (init_location != 0xC339A):
+            raise ValueError("oops.... init function location is fixed in FW build")
+        self.asf.patch(init_bin, init_location, clobber=True)
+        
+        #Entry is not at start of file sometimes in this file?
+        print("IRQ has offset of 0x%x (dealt with)"%irq_offset)
+        irq_location += irq_offset
+        
+        # IRQ vector - at fixed location 0x080402DC so don't need to worry about
+        # this moving. Address needs to be +1 for normal code jump location.
+        irq_location_packed = struct.pack("<I", 0x08000000 + irq_location + 1)
+        self.asf.patch(irq_location_packed, 0x402dc, clobber=True)
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 if __name__ == "__main__":
-    b = open("../stm32.bin", "rb")
+    parser = argparse.ArgumentParser(description='Patch Airsense Firmware with various updates.')
+    parser.add_argument('INFILE', help="Input original binary file")
+    parser.add_argument('OUTFILE', help="Output patched file")
+    
+    parser.add_argument('OPERATION', help="Operation to perform", choices=['INFO', 'PATCH'])
+    
+    patch_list_yn = [
+        {'arg':"patch-bypass-start",    'desc':"Bypass checks that block start-up.",                    'default':True,  'function':'bypass_startcheck'},
+        {'arg':"patch-unlock-uilimits", 'desc':"Unlock higher UI limits.",                              'default':True,  'function':'unlock_ui_limits'},
+        {'arg':"patch-extra-debug",     'desc':"Add extra debug to display.",                           'default':False, 'function':'extra_debug'},
+        {'arg':"patch-extra-modes",     'desc':"Add all modes.",                                        'default':True,  'function':'extra_modes'},
+        {'arg':"patch-extra-menu",      'desc':"Try enabling extra menu items.",                        'default':True,  'function':'extra_menu'},
+        {'arg':"patch-all-menu",        'desc':"All menu items will always be visible.",                'default':False, 'function':'all_menu'},
+        {'arg':"patch-gui-config",      'desc':"Enable all of the editable options in the settings menu.", 'default':True,'function':'gui_config'},
+        {'arg':"patch-logos",           'desc':"Change start-up logos.",                                'default':False,  'function':'patch_logos'},
+        {'arg':"patch-fw-serialmonitor",'desc':"Add monitor binary running on USART3 accessory port.",  'default':False, 'function':'patch_uart3_monitor'},
+        {'arg':"patch-fw-breath",       'desc':"Add breath binary to allow direct pressure control.",   'default':False, 'function':''},
+        {'arg':"patch-fw-graph",        'desc':"Add graph binary to allow graphing of pressures.",      'default':False, 'function':''},
+    ]
+    
+    for arg in patch_list_yn:
+        if arg['default'] == True:
+            choices = ['Y', 'n']
+        else:
+            choices = ['y', 'N']
+        parser.add_argument("--"+arg['arg'], help=arg['desc'], default=arg['default'], choices=choices)
+    
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite output file if it exists already.")
+    
+    args = parser.parse_args()
+
+    #Open existing file
+    b = open(args.INFILE, "rb")
     asf = ASFirmware(b)
     b.close()
+    
+    if args.OPERATION == "PATCH":
 
-    #TODO - need to do SHA256 test, or modify addresses to be more generic
+        patches = ASFirmwarePatches(asf)
+        
+        print("PATCH: Change text on main menu and airplane mode")
+        patches.change_text()
+        
+        for patch in patch_list_yn:
+            if str2bool(getattr(args, patch['arg'].replace("-","_"))):
+                print("PATCH: " + patch['desc'])
+                getattr(patches, patch['function'])()
 
-    asf.patch(b'\xc0\x46', 0xF0, checkempty=True)
-    asf.patch(b'HACKED!', 0x17500, checkempty=True)
-    asf.patch(b'NOT FOR USE\x00', 0x1A540, checkempty=True)
-    asf.patch(b'WARNING! WARNING! Ventilator test firmware: Not for humans!\x00', 0x1B860, checkempty=True)
-
-    #patch_code()
-    unlock_ui_limits()
-    extra_debug()
-    extra_modes()
-    extra_menu()
-    #all_menu()
-    gui_config()
-
-    patch_logos()
-
-    patch_uart3_monitor()
-
-    asf.fix_crcs()
-    asf.write_output("../stm32-unlocked.bin", overwrite=True)
+        asf.fix_crcs()
+        asf.write_output(args.OUTFILE, args.overwrite)

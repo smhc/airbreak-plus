ebf278cfaae3e98a1e508cc4a09442abad875cb3
Asmageddon <asmageddon@gmail.com>
2023-01-09 13:28:26 +0100

    graph.c checkpoint

--- graph.c
+++ graph.c
@@ -4,15 +4,13 @@
 #include "stubs.h"
 #include "state.h"
 
-int start(void)
-{
+int start(void) {
 	// therapy manager variable dictionaries
 	float * const fvars	= (void*) 0x2000e948;
 	int * const ivars	= (void*) 0x2000e750;
 
 	// don't do anything if we are not in an active therapy mode
-	if (ivars[0x6f] == 0)
-		return 0;
+	if (ivars[0x6f] == 0) return 0;
 
 	// break out of the current clipping so we can drawon the entire screen
 	unsigned * const color_ptr = (unsigned*)(gui_context + 60);
@@ -33,10 +31,11 @@ int start(void)
 	*xOff = 0;
 	*yOff = 0;
 
+// Causes the device to crash and reboot
 #if 0
 	GUI_SetColor(0x8);
 	GUI_FillRect(0, 130, 200, 160);
-	
+
 	GUI_SetColor(0xFF0000);
 	GUI_SetFont(font_16);
 	//static const char __attribute__((__section__(".text"))) msg[] = "Hello, world!";
@@ -49,49 +48,51 @@ int start(void)
 	GUI_DispStringAt(buf, 40, 150);
 #endif
 
+
 	// Draw a strip chart
 	const int width = 240;
 	const int center = 190;
 	const int height = 80;
+	// const int vscale = 2; 
+
+	const unsigned pos_x = state->sample;
+	// const unsigned pos_x = (tim_read_tim5()/1000) % width; 
 
 	// actual pressure 0 - 32, scaled to -30 to 30
 	int value = 2 * fvars[2] - height/2;
-	int command = 2 * fvars[0x2d] - height/2;
+	int command = 2 * fvars[0x2a] - height/2;
 
-	if (value < -height/2)
-		value = -height/2;
-	else
-	if (value > +height/2)
-		value = +height/2;
+	if (value < -height/2) value = -height/2;
+	if (value > +height/2) value = +height/2;
 
 	GUI_SetColor(0x0);
-	LCD_FillRect(state->sample, center - height/2, state->sample + 8, center + height/2);
-	// bgr?
+	LCD_FillRect(state->sample, center - height/2, state->sample + 8, center + height/2); // bgr?
 
-	// draw 5, 10, 15, 20 very faintly
+	// draw 0, 5, 10, 15, 20 very faintly
 	GUI_SetColor(0x202020);
-	LCD_DrawPixel(state->sample, center - (2 * 10 - height/2));
-	LCD_DrawPixel(state->sample, center - (2 * 20 - height/2));
-	LCD_DrawPixel(state->sample, center - (2 * 30 - height/2));
+	LCD_DrawPixel(pos_x, center + height/2);
+	LCD_DrawPixel(pos_x, center - (2 * 10 - height/2));
+	LCD_DrawPixel(pos_x, center - (2 * 20 - height/2));
+	LCD_DrawPixel(pos_x, center - (2 * 30 - height/2));
+
+	// Draw the last breath's tidal volume
+	// GUI_SetColor(0xffff00);
+	// LCD_DrawPixel(state->sample, center + height/2 - (int)(fvars[0xBE] / 25.0f) );
+
+	// Draw the cumulative volume stored in unused variable
+	// GUI_SetColor(0xffff00);
+	// LCD_DrawPixel(pos_x, center + height/2 - (int)(fvars[0x19] / 25.0f) );
 
 	// draw the current commanded pressure faintly
 	GUI_SetColor(0x0000F0);
-	LCD_DrawPixel(state->sample, center - command);
+	LCD_DrawPixel(pos_x, center - command);
 
-	// draw thje strip chart in bright green
+	// draw the current actual pressure in bright green
 	GUI_SetColor(0x00FF00);
+	LCD_DrawPixel(pos_x, center - value);
 
-	if (state->last_value < value)
-	{
-		for(int y = state->last_value ; y <= value ; y++)
-			LCD_DrawPixel(state->sample, center - y);
-	} else {
-		for(int y = value ; y <= state->last_value ; y++)
-			LCD_DrawPixel(state->sample, center - y);
-	}
-
-	state->last_value = value;
-	state->sample = (state->sample + 1) % width;
+	// state->last_value = value;
+	state->sample = (pos_x + 1) % width;
 
 	// restore the old clipping rectangle
 	clip[0] = old_x0;
